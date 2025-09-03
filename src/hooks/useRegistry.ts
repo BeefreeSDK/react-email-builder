ba9186dcc9fd7fb32c701614c87786f291108a7a
Roberto Marchetti <marchetti@beefree.io>
2025-09-03 06:48:52 +0200

    chore: moved builderRegistry straight to useRegistry

--- src/hooks/useRegistry.ts
+++ src/hooks/useRegistry.ts
@@ -1,6 +1,8 @@
 import BeefreeSDK from '@beefree.io/sdk'
-import { builderRegistry } from '../registry'
 import { DEFAULT_ID } from '../constants'
+
+// Global registry for the builder; probably no need to export it at all
+export const builderRegistry = new Map()
 
 export const useRegistry = (id = DEFAULT_ID) => [
   builderRegistry,
