3d1a6530d173ab8e1dfe072a436a2e703e07425e
Simone Ferrario <simone.ferrario@beefree.io>
2025-09-10 09:53:08 +0200

    feat(npm-react): updating example

--- example/App.tsx
+++ example/App.tsx
@@ -1,50 +1,123 @@
-import React, { useMemo, useRef, useState } from 'react'
+import React, { useCallback, useMemo, useRef, useState } from 'react'
 import EmailBuilder, { useBuilder } from '../dist/index.es'
 import BeefreeSDK from '@beefree.io/sdk'
 
+const names = ['pippo', 'pluto', 'paperino', 'topolino', 'minnie', 'qui', 'quo', 'qua']
+
 export const App = () => {
   const builderRef = useRef<BeefreeSDK>(null)
-  const [users, setUsers] = useState<string[]>(['pippo'])
+  const [users, setUsers] = useState<string[]>([names[0]])
   const [debug, setDebug] = useState<boolean>(false)
+  const [savedRows, setSavedRows] = useState([])
   const { save: saveHtml } = useBuilder(builderRef)
   const handleSave = () => {
-    setUsers(['pippo', 'pluto'])
     saveHtml()
   }
 
-  const config = useMemo(() => ({
-    id: 'test',
-    hooks: {
-      getMentions: {
-        handler: async (resolve) => {
-          resolve(users.map(user => ({
-            userColor: '#ff0000',
-            username: user,
-            value: user,
-            uid: user,
-          })),
-          )
+  const handleUsers = () => {
+    if (users.length < names.length) setUsers(prevUsers => [...prevUsers, names[prevUsers.length]])
+  }
+
+  const getMentionsHandler = useCallback(async (resolve) => {
+    resolve(users.map(user => ({
+      userColor: '#ff0000',
+      username: user,
+      value: user,
+      uid: user,
+    })),
+    )
+  }, [users])
+
+  const getRowsHandler = useCallback(async (resolve, reject, args) => {
+    const { handle } = args
+    switch (handle) {
+      case 'saved-rows':
+        resolve(savedRows)
+        break
+      case 'custom-rows':
+        resolve([{ name: 'One button', columns: [{ weight: 12, modules: [{ type: 'button' }] }] }])
+        break
+      default:
+        reject(`${handle} not handled`)
+    }
+  }, [savedRows])
+
+  const onSaveRowHandler = (savedRow) => {
+    console.log(`%c[APP] - onSaveRow ->`, `color:${'#00ff00'}`, savedRow)
+    setSavedRows(prevRows => [...prevRows, JSON.parse(savedRow)])
+  }
+
+  const saveRowHandler = useCallback(async (resolve) => {
+    resolve({ name: 'row' })
+  }, [])
+
+  const errorHandler = useCallback((error) => {
+    console.log(`%c[APP] - onError ->`, `color:${'#ff0000'}`, error)
+  }, [])
+
+  const warningHandler = useCallback((warning) => {
+    console.log(`%c[APP] - onWarning ->`, `color:${'#fbda00'}`, warning)
+  }, [])
+
+  const config = useMemo(() => {
+    console.log(`%c[APP] - config ->`, `color:${'#a100ff'}`, 'config object re-created')
+    return ({
+      id: 'test',
+      hooks: {
+        getMentions: {
+          handler: getMentionsHandler,
+        },
+        getRows: {
+          handler: getRowsHandler,
         },
       },
-    },
-    contentDialog: {
-      saveRow: {
-        label: 'Save',
-        handler: async (resolve) => {
-          resolve({ name: 'row' })
+      contentDialog: {
+        addOn: {
+          // FIXME: purposely made it to be recreated at every render to test the new loader
+          handler: (resolve) => {
+            resolve({ type: 'button', value: { text: 'Button' } })
+          },
+        },
+        saveRow: {
+          label: 'Save',
+          handler: saveRowHandler,
         },
       },
-    },
-    debug: {
-      all: debug,
-    },
-  }), [users, debug])
+      debug: {
+        all: debug,
+      },
+      onSaveRow: onSaveRowHandler,
+      onError: errorHandler,
+      onWarning: warningHandler,
+      rowsConfiguration: {
+        emptyRows: true,
+        defaultRows: true,
+        externalContentURLs: [
+          {
+            name: 'Saved rows',
+            handle: 'saved-rows',
+            isLocal: true,
+          },
+          {
+            name: 'Custom rows',
+            handle: 'custom-rows',
+            isLocal: true,
+            behaviors: {
+              canEdit: false,
+              canDelete: false,
+            },
+          },
+        ],
+      },
+    })
+  }, [getMentionsHandler, getRowsHandler, saveRowHandler, debug, errorHandler, warningHandler])
 
   return (
     <>
       <h1>Beefree SDK React Demo</h1>
       <p>Welcome to the Beefree SDK React Demo</p>
       <button onClick={handleSave}>Save</button>
+      <button onClick={handleUsers}>Update users</button>
       <input id="debug_chk" type="checkbox" checked={debug} onChange={e => setDebug(e.target.checked)} />
       <label htmlFor="debug_chk">Debug</label>
       <div id="email-builder">
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
-export const BEEPLUGIN_URL = 'https://pre-bee-app-rsrc.s3.amazonaws.com/plugin/v2/BeePlugin.js'
-export const BEE_AUTH_URL = 'https://pre-bee-auth.getbee.info/loginV2'
+export const BEEPLUGIN_URL = 'https://qa-bee-loader.getbee.io/v2/api/loader_beta'
+// export const BEEPLUGIN_URL = 'http://local-sdk.beefree.io/BeeLoader.js'
+export const BEE_AUTH_URL = 'https://qa-bee-auth.getbee.io/loginV2'
 export const DEFAULT_ID = 'beefree-container'
